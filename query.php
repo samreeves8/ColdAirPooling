d68f3d23950738c03b7a757439b8697acbb2acab
mwm929 <michaelmw929@gmail.com>
2023-03-08 09:47:56 -0700

    Added echo statement

--- query.php
+++ query.php
@@ -71,16 +71,16 @@
         }
         
         foreach($sensors as $sensor){
-            $sql = "SELECT Temperature FROM ".$table." WHERE Sensor IS ".$sensor." AND Datetime BETWEEN ".$dateTimeStart." AND ".$dateTimeEnd." AND Temperature BETWEEN ".$tempMin." AND " .$tempMax."";
+            $sql = "SELECT Temperature FROM ".$table." WHERE Sensor IS ".$sensor." AND DateTime BETWEEN ".$dateTimeStart." AND ".$dateTimeEnd." AND Temperature BETWEEN ".$tempMin." AND " .$tempMax."";
             echo $sql;
-            $stmt = $conn->prepare("SELECT Temperature FROM ? WHERE Sensor IS ? AND Datetime BETWEEN ? AND ? AND Temperature BETWEEN ? AND ?;");
+            $stmt = $conn->prepare("SELECT Temperature FROM ? WHERE Sensor IS ? AND DateTime BETWEEN ? AND ? AND Temperature BETWEEN ? AND ?;");
             $stmt->bind_param("ssssdd", $table, $sensor, $dateTimeStart, $dateTimeEnd, $tempMin, $tempMax);
             $stmt->execute();
             $result = $stmt->get_result();
 
             if ($result->num_rows > 0) {
                 while ($row = $result->fetch_assoc()) {
-                    echo $row;
+                    echo "Temp at " . $row["Datetime"] . " = " . $row["Temperature"];
                 }
             }
 
