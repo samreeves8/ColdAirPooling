491f983993e488c328fad1b74aa9e07e9ca722a7
KevinCabral401 <KevinCabral401@gmail.com>
2023-03-20 17:59:08 -0600

    Added Second Sensor

--- graph.php
+++ graph.php
@@ -2,6 +2,8 @@
 
     $temps = array();  
     $dates = array();
+    $temps2 = array();
+    $dates2 = array();
 
     $conn = new mysqli('localhost', 'gunniso1_Admin', 'gunnisoncoldair', 'gunniso1_SensorData');
     if ($conn->connect_error) {
@@ -26,6 +28,25 @@
     $myArrayJsonDates = json_encode($dates);
     echo "<script>var temps = JSON.parse('" . $myArrayJsonTemps . "');</script>";
     echo "<script>var dates = JSON.parse('" . $myArrayJsonDates . "');</script>";
+
+    $sql = "SELECT Temperature, DateTime FROM TempData WHERE Sensor = '06MYS' 
+            AND DateTime BETWEEN '2022-12-20 00:00:00' AND '2022-12-21 00:00:00';";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    echo $sql."<br>";
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $temps2[] = $row["Temperature"];
+            $dates2[] = $row["DateTime"]; 
+        }
+    }
+
+    $myArrayJsonTemps2 = json_encode($temps2);
+    $myArrayJsonDates2 = json_encode($dates2);
+    echo "<script>var temps2 = JSON.parse('" . $myArrayJsonTemps2 . "');</script>";
+    echo "<script>var dates2 = JSON.parse('" . $myArrayJsonDates2 . "');</script>";
 ?>
 
 
@@ -40,8 +61,8 @@
     <canvas id="myChart"></canvas>
     <script>
         temps = temps.map(Number);
-
-        console.log(temps);
+        temps2 = temps2.map(Number);
+    
         new Chart("myChart", {
             
             type: "line",
@@ -50,6 +71,10 @@
                 datasets: [{
                     data: temps,
                     borderColor: "red",
+                    fill: false
+                },{
+                    data: temps2,
+                    borderColor: "blue",
                     fill: false
                 }]
             },
