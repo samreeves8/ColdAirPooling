7f98870e53e704dbfa9c2518d2992276ef9d030b
KevinCabral401 <KevinCabral401@gmail.com>
2023-03-20 17:37:37 -0600

    Added PHP connection, stored query results in JS

--- graph.php
+++ graph.php
@@ -1,3 +1,34 @@
+<?php
+
+    $temps = array();  
+    $dates = array();
+
+    $conn = new mysqli('localhost', 'gunniso1_Admin', 'gunnisoncoldair', 'gunniso1_SensorData');
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT Temperature, DateTime FROM TempData WHERE Sensor = '05VAN' 
+            AND DateTime BETWEEN '2022-12-20 00:00:00' AND '2022-12-21 00:00:00';"
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    echo $sqlString."<br>";
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $temps[] = $row["Temperature"];
+            $dates[] = $row["DateTime"]; 
+        }
+    }
+
+    $myArrayJsonTemps = json_encode($temps);
+    $myArrayJsonDates = json_encode($dates);
+    echo "<script>var temps = JSON.parse('" . $myArrayJsonTemps . "');</script>";
+    echo "<script>var dates = JSON.parse('" . $myArrayJsonDates . "');</script>";
+?>
+
+
 <!DOCTYPE html>
 <html>
   <head>
