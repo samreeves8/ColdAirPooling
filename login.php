e034a24bdfe9f27f6ec38831b3a79507f7b94919
samreeves8 <samreeves8@gmail.com>
2023-03-24 09:40:28 -0600

    redirection

--- login.php
+++ login.php
@@ -62,9 +62,9 @@ catch(PDOException $e){
     echo "Connection Failed: " . $e->getMessage();
 }
 
-if ( !isset($_POST['username'], $_POST['password']) ) {
-	exit('Please fill both the username and password fields!');
-}
+//if ( !isset($_POST['username'], $_POST['password']) ) {
+	//exit('Please fill both the username and password fields!');
+//}
 
 if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
 	// Bind parameters.
