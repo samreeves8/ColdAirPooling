9bc6bbdaaecbabe289f0499162c5460a2baa254a
samreeves8 <samreeves8@gmail.com>
2023-03-24 10:12:40 -0600

    admin page work

--- admin.php
+++ admin.php
@@ -1,5 +1,11 @@
 <?php
+session_start();
 
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    // user is not logged in, redirect to login page
+    header('Location: login.php');
+    exit;
+}
 
 ?>
 
