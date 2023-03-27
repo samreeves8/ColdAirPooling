d9e2e6237cf307610ee0ed341a03e120bbd3abb8
mwm929 <michaelmw929@gmail.com>
2023-03-27 10:10:10 -0600

    test

--- queryTest.php
+++ queryTest.php
@@ -28,7 +28,7 @@
         if($currentFormIndex > 0){
             echo "<input type = 'submit' name = 'previous' value = 'Previous'>";
         }
-        if($currentFormIndex < 3){
+        if($currentFormIndex < 2){
             echo "<input type = 'submit' name = 'next' value = 'Next'>";
         }else{
             echo "<input type = 'submit' name = 'graph' value = 'Graph'>";
