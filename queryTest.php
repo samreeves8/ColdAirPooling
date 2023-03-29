008f42b32ce9fb80dc1d1984c07d3a90d09243a2
mwm929 <michaelmw929@gmail.com>
2023-03-29 09:09:20 -0600

    date test

--- queryTest.php
+++ queryTest.php
@@ -27,10 +27,7 @@
             <input type="time" id="timeStart" name="timeStart">
             <br>
             <label for="dateEnd">Select an end date:</label>
-            <input type="date" id="dateEnd" name="dateEnd">
-            <script>
-                document.getElementById("dateEnd").value = new Date().toDateInputValue();
-            </script>
+            <input type="date" id="dateEnd" name="dateEnd" value = "<?php echo date('Y-m-d'); ?>">
             <label for="timeEnd">Select an end time:</label>
             <input type="time" id="timeEnd" name="timeEnd">
             <br>'
