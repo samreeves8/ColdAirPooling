59a58ad0ee7263036d1d063bc9cdf95b5a3b82c9
mwm929 <michaelmw929@gmail.com>
2023-03-29 19:27:18 -0600

    fix

--- queryTest.php
+++ queryTest.php
@@ -76,7 +76,7 @@
                 });
             }
 
-            addMarker(\'01OBS\', 38.50949, -106.93991, \'02/16/2022\', \'Yes\', mymap, sensorSet);
+            addMarker(\'01OBS\', 38.50949, -106.93991, 7457, \'02/16/2022\', \'Yes\', mymap, sensorSet);
             
 
 
