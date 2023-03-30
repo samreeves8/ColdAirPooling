3cfd4af2c327cf39695f4a19bf77e3b3e788d391
samreeves8 <samreeves8@gmail.com>
2023-03-29 20:23:08 -0600

    more nav bar fixes

--- admin.php
+++ admin.php
@@ -107,6 +107,7 @@ if (isset($_POST['toDelete'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="nav.css">
     <title>Document</title>
 </head>
 <body>
