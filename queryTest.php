e03a89a14d82f6f3dc94ec235505c770b6b537c1
mwm929 <michaelmw929@gmail.com>
2023-03-30 18:19:46 -0600

    added function

--- queryTest.php
+++ queryTest.php
@@ -81,9 +81,9 @@
                     <br>\
                     Current sensors selected: \' + temp + \'\
                     <br>\
-                    <button type = "button" onclick="removeFromSet(" + id + "," + sensorSet + ")">Remove from set</button>\
+                    <button type = "button" onclick="sensor.delete(" + id + ")">Remove from set</button>\
                     <br>\
-                    <button type = "button" onclick="addToSet(" + id + "," + sensorSet + ")">Add to set</button>\
+                    <button type = "button" onclick="sensor.add(" + id + ")">Add to set</button>\
                     <br>\
                     <\p>\';
                 });
