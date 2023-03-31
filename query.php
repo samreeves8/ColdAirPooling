fb8ac640cacbad526a9504cc6f1de753e6400b59
KevinCabral401 <KevinCabral401@gmail.com>
2023-03-30 19:53:04 -0600

    moved JS outside

--- query.php
+++ query.php
@@ -20,6 +20,34 @@
          </ul>
     </div>
 </body>
+        <script>
+            function rangeSelected() {
+                const mySelect = document.getElementById('range');
+                const myForm = document.getElementById('rangeForm');
+                
+
+                mySelect.addEventListener('change', function(){
+                    var val = mySelect.value;
+                    myForm.submit();
+                    // Create a new XMLHttpRequest object
+                    var xhr = new XMLHttpRequest();
+    
+                    // Define the PHP script URL and the request method
+                    var url = 'query.php';
+                    var method = 'POST';
+    
+                    // Define the data to be sent to the PHP script
+                    var data = 'val=' + encodeURIComponent(val);
+    
+                    // Set up the request headers
+                    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    
+                    // Send the request to the PHP script
+                    xhr.open(method, url, true);
+                    xhr.send(data);            
+                });
+            }
+        </script>
 </html>
 
 <?php
@@ -130,35 +158,6 @@
         }
         echo "</select><br></form>";
 
-        echo 
-        "<script>
-            function rangeSelected() {
-                const mySelect = document.getElementById('range');
-                const myForm = document.getElementById('rangeForm');
-                
-
-                mySelect.addEventListener('change', function(){
-                    var val = mySelect.value;
-                    myForm.submit();
-                    // Create a new XMLHttpRequest object
-                    var xhr = new XMLHttpRequest();
-    
-                    // Define the PHP script URL and the request method
-                    var url = 'query.php';
-                    var method = 'POST';
-    
-                    // Define the data to be sent to the PHP script
-                    var data = 'val=' + encodeURIComponent(val);
-    
-                    // Set up the request headers
-                    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    
-                    // Send the request to the PHP script
-                    xhr.open(method, url, true);
-                    xhr.send(data);            
-                });
-            }
-        </script>";
 
         // Get the value of the "val" parameter from the POST request
         $val = $_POST['val'] ?? NULL;
