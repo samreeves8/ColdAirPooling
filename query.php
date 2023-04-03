4c9a2c2fcec55fb8fe4f73c0ffacbd7123d6c191
KevinCabral401 <KevinCabral401@gmail.com>
2023-04-03 09:57:00 -0600

    prints

--- query.php
+++ query.php
@@ -114,6 +114,7 @@
         }
 
         $serializedArray = serialize($sensors);
+        print_r($sensors);
 
         echo "<form id = 'rangeForm' action='query.php' method='POST'><br><select id = 'range' style = 'font-size: 24px;' onchange='rangeSelected()'>";
         $counter = 0;
