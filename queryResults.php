652fc3712f85ab8e69c005bc6ef6c46973f85cad
KevinCabral401 <KevinCabral401@gmail.com>
2023-04-03 11:06:06 -0600

    added X logic

--- queryResults.php
+++ queryResults.php
@@ -10,6 +10,33 @@
         if ($val !== null) {
           // Do something with the value
           echo "The value is: " . $val;
+            if($val == "3 Minutes"){
+                $x = 1;
+            } else if($val == "6 Minutes"){
+                $x = 3;
+            } else if($val == "15 Minutes"){
+                $x = 6;
+            } else if($val == "30 Minutes"){
+                $x = 11;
+            } else if($val == "1 Hour"){
+                $x = 21;
+            } else if($val == "2 Hours"){
+                $x = 41;
+            } else if($val == "4 Hours"){
+                $x = 81;
+            } else if($val == "12 Hours"){
+                $x = 241;
+            } else if($val == "Daily"){
+                $x = 481;
+            } else if($val == "Bi-Daily"){
+                $x = 961;
+            } else if($val == "Weekly"){
+                $x = 3361;
+            } else if($val == "Bi-Weekly"){
+                $x = 6721;
+            } else {
+                // handle any other cases or errors here
+            }
         }
 
         foreach($unserializedArray as $sensor){
@@ -20,7 +47,7 @@
                 $table = "TempData";
             }
     
-            $sqlString = "SELECT Sensor, FLOOR((@row_number:=@row_number+1)/$val) AS GroupNum, MIN(DateTime) AS StartDateTime, MAX(DateTime) AS EndDateTime, 
+            $sqlString = "SELECT Sensor, FLOOR((@row_number:=@row_number+1)/$x) AS GroupNum, MIN(DateTime) AS StartDateTime, MAX(DateTime) AS EndDateTime, 
             MIN(Temperature) AS MinTemperature, MAX(Temperature) AS MaxTemperature, ROUND(AVG(Temperature),2) AS AvgTemperature 
             FROM $table, (SELECT @row_number:=0) AS t WHERE Sensor IN ('$sensor') AND DateTime BETWEEN '$dateTimeStart' AND '$dateTimeEnd' GROUP BY Sensor, GroupNum  ORDER BY `Sensor`  DESC;";
     
