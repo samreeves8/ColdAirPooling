dc0c0f1fa5e3e9b585f0c8e41b78fd6b3ed43eab
KevinCabral401 <KevinCabral401@gmail.com>
2023-04-03 10:38:00 -0600

    syntax

--- queryResults.php
+++ queryResults.php
@@ -20,7 +20,7 @@
                 $table = "TempData";
             }
     
-            $sqlString = "SELECT Sensor, FLOOR((@row_number:=@row_number+1)/'$val') AS GroupNum, MIN(DateTime) AS StartDateTime, MAX(DateTime) AS EndDateTime, 
+            $sqlString = "SELECT Sensor, FLOOR((@row_number:=@row_number+1)/$val) AS GroupNum, MIN(DateTime) AS StartDateTime, MAX(DateTime) AS EndDateTime, 
             MIN(Temperature) AS MinTemperature, MAX(Temperature) AS MaxTemperature, ROUND(AVG(Temperature),2) AS AvgTemperature 
             FROM $table, (SELECT @row_number:=0) AS t WHERE Sensor IN ('$sensor') AND DateTime BETWEEN '$dateTimeStart' AND '$dateTimeEnd' GROUP BY Sensor, GroupNum  ORDER BY `Sensor`  DESC;";
     
