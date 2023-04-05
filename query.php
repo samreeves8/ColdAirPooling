abe391fc186bf422b1e455551ecec9647e9f922a
mwm929 <michaelmw929@gmail.com>
2023-04-05 17:43:56 -0600

    migrated from query test

--- query.php
+++ query.php
@@ -9,6 +9,8 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
     <title>Document</title>
 </head>
 <body>
