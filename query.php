e6561e3b8049e7a73b0b4cfc95ec1421484a8998
mwm929 <michaelmw929@gmail.com>
2023-04-05 18:37:55 -0600

    style

--- query.php
+++ query.php
@@ -11,6 +11,7 @@ session_start();
     <link rel="stylesheet" href="nav.css">
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+    <link rel = "stylesheet" href = "query.css">
     <title>Document</title>
 </head>
 <body>
