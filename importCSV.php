2cb6bfc7bf081e475b90c9092395f4883c3742f1
samreeves8 <samreeves8@gmail.com>
2023-04-10 11:02:24 -0600

    fixed stylesheets

--- importCSV.php
+++ importCSV.php
@@ -8,8 +8,8 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="nav.css">
-    <link rel="stylesheet" href="import.css">
+    <link rel="stylesheet" href="styles/nav.css">
+    <link rel="stylesheet" href="styles/import.css">
     <title>Document</title>
 </head>
 
--- login.php
+++ login.php
@@ -13,8 +13,8 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === 1) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="nav.css">
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="styles/nav.css">
+    <link rel="stylesheet" href="styles/login.css">
     <title>Document</title>
 </head>
 <body>
