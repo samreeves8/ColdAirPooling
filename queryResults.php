97c74f19b3f5ea1341174b64816d28f53cace04b
KevinCabral401 <KevinCabral401@gmail.com>
2023-04-10 10:26:00 -0600

    x axis changes

--- queryResults.php
+++ queryResults.php
@@ -49,7 +49,16 @@
     <title>Query Results</title>
 </head>
 <body>
-    <?php include 'navBar.php';?>
+    <div class="navbar">
+         <ul class="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="query.php">Query</a></li>
+            <li><a href="#">Members</a></li>
+            <li><a href="login.php">Log In</a></li>
+         </ul>
+    </div>
 </body>
 </html>
 
@@ -201,12 +210,12 @@
         // Replace the original arrays with the updated arrays
         $allArrays = array_values($allArrays);
 
-        //Code to display graph
+        // Code to display graph
         if (empty($allArrays)) {
             echo "No Data Found";
         } else {
             $data = json_encode($allArrays);
-                    
+            
             echo '<canvas id="myChart"></canvas>;';
             echo'<script>
                 var allArrays = '.$data.';
@@ -221,7 +230,7 @@
                         fill: false
                     });
                 }
-        
+
                 new Chart("myChart", {
                     type: "line",
                     data: {
@@ -229,10 +238,25 @@
                         datasets: datasets
                     },
                     options: {
-                        legend: {display: true}
+                        legend: {display: true},
+                        scales: {
+                            xAxes: [{
+                                type: "time",
+                                time: {
+                                    unit: "hour",
+                                    displayFormats: {
+                                        hour: "MMM D hA"
+                                    },
+                                    tooltipFormat: "MMM D hA"
+                                },
+                                ticks: {
+                                    source: "labels"
+                                }
+                            }]
+                        }
                     }
                 });
-        
+
                 function getRandomColor() {
                     var letters = "0123456789ABCDEF";
                     var color = "#";
