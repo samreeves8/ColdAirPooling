17ce605a825061775710ae7f77e0fd65d79d1f83
KevinCabral401 <KevinCabral401@gmail.com>
2023-04-10 18:40:02 -0600

    moved styling back out for testing

--- queryResults.php
+++ queryResults.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="nav.css">
-    <link rel = "stylesheet" href = "styles/query.css">
+    <link rel = "stylesheet" href = "query.css">
     <link rel = "stylesheet" href = "styles/table.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 
