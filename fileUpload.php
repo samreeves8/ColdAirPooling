ce7a25b1522b1c14cd3522fb57988dc18e480238
KevinCabral401 <KevinCabral401@gmail.com>
2023-04-11 20:52:13 -0600

    change

--- fileUpload.php
+++ fileUpload.php
@@ -15,58 +15,60 @@
     <div class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">0%</div>
   </div>
 
-  <script>
-    $(document).ready(function() {
-      $('#upload-form').on('submit', function(event) {
-        event.preventDefault();
-        console.log("successfully prevented default");
-        var formData = new FormData($('#upload-form')[0]);
-        var totalBytes = 0;
-        for (var i = 0; i < formData.getAll('files[]').length; i++) {
-          totalBytes += formData.getAll('files[]')[i].size;
+<script>
+$(document).ready(function() {
+  $('#upload-form').on('submit', function(event) {
+    event.preventDefault();
+    console.log("successfully prevented default");
+    var formData = new FormData($('#upload-form')[0]);
+    var totalBytes = 0;
+    for (var i = 0; i < formData.getAll('files[]').length; i++) {
+      totalBytes += formData.getAll('files[]')[i].size;
+    }
+    var files = formData.getAll('files[]');
+    var currentIndex = 0;
+    uploadNextFile(files, currentIndex, totalBytes);
+  });
+});
+
+function uploadNextFile(files, index, totalBytes) {
+  var formData = new FormData();
+  formData.append('file', files[index]);
+  $.ajax({
+    url: 'fileUpload.php',
+    type: 'POST',
+    data: formData,
+    processData: false,
+    contentType: false,
+    xhr: function() {
+      var xhr = new XMLHttpRequest();
+      xhr.withCredentials = true;
+      xhr.upload.addEventListener('progress', function(event) {
+        if (event.lengthComputable) {
+          var bytesUploaded = event.loaded;
+          var percentComplete = bytesUploaded / totalBytes * 100;
+          console.log(percentComplete);
+          $('.progress-bar').width(percentComplete + '%').html(Math.round(percentComplete) + '%');
         }
-        $.ajax({
-          url: 'fileUpload.php',
-          type: 'POST',
-          data: formData,
-          processData: false,
-          contentType: false,
-          xhr: function() {
-            var xhr = new XMLHttpRequest();
-            xhr.withCredentials = true;
-            xhr.upload.addEventListener('progress', function(event) {
-              if (event.lengthComputable) {
-                var bytesUploaded = 0;
-                for (var i = 0; i < formData.getAll('files[]').length; i++) {
-                  if (event.loaded >= bytesUploaded + formData.getAll('files[]')[i].size) {
-                    console.log(bytesUploaded += formData.getAll('files[]')[i].size);
-                    bytesUploaded += formData.getAll('files[]')[i].size;
-                  } else {
-                    bytesUploaded += event.loaded - bytesUploaded;
-                    break;
-                  }
-                }
-                var percentComplete = bytesUploaded / totalBytes * 100;
-                console.log(percentComplete);
-                $('.progress-bar').width(percentComplete + '%').html(Math.round(percentComplete) + '%');
-              }
-            }, false);
-            return xhr;
-          },
-          success: function(response) {
-            console.log(response);
-          }
-        });
-      });
-    });
-  </script>
+      }, false);
+      return xhr;
+    },
+    success: function(response) {
+      console.log(response);
+      index++;
+      if (index < files.length) {
+        uploadNextFile(files, index, totalBytes);
+      }
+    }
+  });
+}
+</script>
 </body>
 </html>
 
 
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
     // FTP server details
     $ftp_server = "ftp.gunnisoncoldpooling.net";
     $ftp_username = "Admin@gunnisoncoldpooling.net";
@@ -79,29 +81,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $login_result = ftp_login($conn_id, $ftp_username, $ftp_password);
 
     if ($login_result) {
-    // turn on passive mode transfers
-    ftp_pasv($conn_id, true);
+        // turn on passive mode transfers
+        ftp_pasv($conn_id, true);
 
-    // loop through uploaded files
-    foreach ($_FILES["files"]["tmp_name"] as $key => $tmp_name) {
-        // get the local file path
-        $local_file = $_FILES["files"]["tmp_name"][$key];
+        // loop through uploaded files
+        foreach ($_FILES["files"]["tmp_name"] as $key => $tmp_name) {
+            // get the local file path
+            $local_file = $_FILES["files"]["tmp_name"][$key];
 
-        // get the original file name
-        $file_name = $_FILES["files"]["name"][$key];
+            // get the original file name
+            $file_name = $_FILES["files"]["name"][$key];
 
-        // upload the file to FTP server
-        $upload_result = ftp_put($conn_id, $file_name, $local_file, FTP_BINARY);
+            // open the file for reading
+            $handle = fopen($local_file, 'r');
 
-        if (!$upload_result) {
-        echo "Upload failed: " . $file_name . "<br>";
+            // upload the file to FTP server
+            $upload_result = ftp_fput($conn_id, $file_name, $handle, FTP_BINARY);
+
+            if (!$upload_result) {
+                echo "Upload failed: " . $file_name . "<br>";
+            } else {
+                // process the file here
+                // ...
+            }
+
+            // close the file handle
+            fclose($handle);
         }
-    }
 
-    // close the FTP connection
-    ftp_close($conn_id);
+        // close the FTP connection
+        ftp_close($conn_id);
     } else {
-    echo "Login failed.";
+        echo "Login failed.";
     }
 }
 ?>
