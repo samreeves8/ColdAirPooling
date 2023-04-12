b87a0f1b36052eed807bfa4941fd9fba4842c44a
KevinCabral401 <KevinCabral401@gmail.com>
2023-04-12 11:02:08 -0600

    database connect

--- fileUpload.php
+++ fileUpload.php
@@ -1,4 +1,33 @@
 <?php
+
+$conn = new mysqli('localhost', 'gunniso1_Admin', 'gunnisoncoldair', 'gunniso1_SensorData');
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+
+$humidity = array("01OBS", "10NEM", "17WIL", "21ALM", "24CAM", "29CAB");  // Humidity Sensors
+    
+//Bind indices for excel documents
+$dateTimeIndex=1;
+$tempIndex = 2;
+$rhIndex = 3;
+$dewPointIndex = 4;
+
+$Sensor = NULL;
+$DateTime = NULL;
+$Temperature = NULL;
+$RH = NULL;
+$DewPoint = NULL;
+$h = NULL;
+
+$sql_humidity = "INSERT INTO HumidData (Sensor, DateTime, Temperature, RH, DewPoint) VALUES (?, ?, ?, ?, ?)";
+$stmt_humidity = mysqli_prepare($conn, $sql_humidity);
+mysqli_stmt_bind_param($stmt_humidity, "ssddd", $Sensor, $DateTime, $Temperature, $RH, $DewPoint);
+
+$sql_temp = "INSERT INTO TempData (Sensor, DateTime, Temperature) VALUES (?, ?, ?)";
+$stmt_temp = mysqli_prepare($conn, $sql_temp);
+mysqli_stmt_bind_param($stmt_temp, "ssd", $Sensor, $DateTime, $Temperature);
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // FTP server details
@@ -24,6 +53,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         // open the local file for reading
         $handle = fopen($local_file, "r");
+        
+        //run database insertion
+        parseData($handle, $file_name);
 
         // initiate the upload
         $upload_result = ftp_nb_fput($conn_id, $file_name, $handle, FTP_BINARY);
@@ -49,6 +81,59 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     } else {
         echo "Login failed.";
     }
+}
+
+function parseData($handle, $file_name) {
+  $Sensor = substr($file_name, 0, 5);
+
+  //Checks which table to access (HumidData or TempData)
+  if(in_array($Sensor, $humidity)){
+    $stmt = $stmt_humidity;
+    $h = true;
+    
+  } else {
+    $stmt = $stmt_temp;
+    $h = false;
+  }
+
+  //Skip the first line
+  fgetcsv($handle);
+
+  while (($row = fgetcsv($handle)) !== false) {  
+    //Accounts for date time differences
+    $DateTime = DateTime::createFromFormat('m/d/Y H:i:s', $row[$dateTimeIndex]);
+    if (!$DateTime) {
+      $DateTime = DateTime::createFromFormat('m/d/Y H:i', $row[$dateTimeIndex]);
+        if (!$DateTime) {
+          $DateTime = DateTime::createFromFormat('m-d-Y H:i:s', $row[$dateTimeIndex]);
+            if (!$DateTime) {
+              $DateTime = DateTime::createFromFormat('m-d-Y H:i', $row[$dateTimeIndex]);
+                if (!$DateTime) {
+                  echo "Invalid file format";
+                    exit();
+                }
+            }
+        }
+    }
+    // Set the parameter values
+    $DateTime = $DateTime->format('Y-m-d H:i:s');
+    $Temperature = $row[$tempIndex];
+    if($h && $Temperature!=null){
+      $RH = $row[$rhIndex];
+      $DewPoint = $row[$dewPointIndex];
+      mysqli_stmt_execute($stmt);    
+
+    } else if(!$h && $Temperature!=null) {
+        mysqli_stmt_execute($stmt);
+    }
+  }
+
+  // Close the statement and connection
+  fclose($file);
+  
+  $conn->commit();
+  mysqli_stmt_close($stmt);
+  mysqli_close($conn);
 }
 ?>
 
