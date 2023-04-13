06810377ae5ff8d1554d5c5fa23d42d0cbc1dbfc
KevinCabral401 <KevinCabral401@gmail.com>
2023-04-12 23:57:59 -0600

    css

--- queryResults.php
+++ queryResults.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="nav.css">
+    <link rel="stylesheet" href="styles/nav.css">
     <link rel = "stylesheet" href = "styles/query.css">
     <link rel = "stylesheet" href = "styles/table.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
