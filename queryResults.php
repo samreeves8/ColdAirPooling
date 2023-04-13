d3c23a0df479f740b9ae78984f73bdd55427a2f4
KevinCabral401 <KevinCabral401@gmail.com>
2023-04-13 12:44:50 -0600

    lblah

--- queryResults.php
+++ queryResults.php
@@ -335,7 +335,9 @@
                         echo "<td>" . $sensor . "</td>";
 
                         $dateTime = new DateTime($row["DateTime"]);
-                        $formattedDateTime = $dateTime->format('M d, Y h:i a');
+                        $roundedDateTime = clone $dateTime; // create a copy of the original DateTime object
+                        $roundedDateTime->modify('-' . $roundedDateTime->format('i') % 5 . ' minutes'); // round down to nearest 5th minute
+                        $formattedDateTime = $roundedDateTime->format('M d, Y h:i a');
 
                         echo "<td>" . $formattedDateTime . "</td>";
                         echo "<td>" . $row["Temperature"] . "</td>";
