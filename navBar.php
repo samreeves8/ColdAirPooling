013d364bd2cdcaec7d0d9948463bbd2321e868b4
KevinCabral401 <KevinCabral401@gmail.com>
2023-04-13 00:11:06 -0600

    changed link to fileUpload

--- navBar.php
+++ navBar.php
@@ -5,7 +5,7 @@
        <li><a href="#">Contact</a></li>
        <?php
        if (isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == 1) {
-           echo '<li><a href="importCSV.php">Import CSV</a></li>';
+           echo '<li><a href="fileUpload.php">Import CSV</a></li>';
         }
         ?>
        <li><a href="query.php">Query</a></li>
