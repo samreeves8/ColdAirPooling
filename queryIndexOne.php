3ae6b6aca8a32109d531331302b0a21cba6edd5d
KevinCabral401 <KevinCabral401@gmail.com>
2023-04-19 17:57:09 -0600

    changes

--- queryIndexOne.php
+++ queryIndexOne.php
@@ -1,4 +1,31 @@
 
+<?php
+        global $conn;
+        $markers = array();
+        
+        $sql = "SELECT Sensor, Latitude, Longitude, Elevation, DATE_FORMAT(Date, '%Y-%m-%d') as Date, humidity FROM SensorData";
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $markers[] = array(
+                    "id" => $row['Sensor'],
+                    "lat" => $row['Latitude'],
+                    "lng" => $row['Longitude'],
+                    "elevation" => $row['Elevation'],
+                    "dateInstalled" => $row['Date'],
+                    "recordsHumidity" => $row['humidity']
+                );
+            }
+        }
+        header('Content-Type: application/json');
+        echo json_encode($markers);
+        exit();
+        
+    ?>
+
 <body>
     <div id="map" style="height: 540px; width: 860px;"></div>
     <div id="sidebarContainer">
@@ -86,33 +113,7 @@
         });
     }
 
-    markers = [];
-    <?php
-        global $conn;
-        $markers = array();
-        
-        $sql = "SELECT Sensor, Latitude, Longitude, Elevation, DATE_FORMAT(Date, '%Y-%m-%d') as Date, humidity FROM SensorData";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $markers[] = array(
-                    "id" => $row['Sensor'],
-                    "lat" => $row['Latitude'],
-                    "lng" => $row['Longitude'],
-                    "elevation" => $row['Elevation'],
-                    "dateInstalled" => $row['Date'],
-                    "recordsHumidity" => $row['humidity']
-                );
-            }
-        }
-        
-        echo json_encode($markers);
-        
-    ?>
-            
+    markers = [];       
     window.onload = function() {
         // create XMLHttpRequest object
         var xhr = new XMLHttpRequest();
