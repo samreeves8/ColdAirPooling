72d2714611fcdd1931aa02c91579e1d9f4b2f03b
samreeves8 <samreeves8@gmail.com>
2023-04-21 09:14:47 -0600

    nav css

--- navBar.php
+++ navBar.php
@@ -1,22 +1,22 @@
 <div class="navbar">
     <ul class="menu">
-       <li><a href="/">Home</a></li>
-       <li><a href="about.php">About</a></li>
-       <li><a href="#">Contact</a></li>
-       <?php
-       if (isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == 1) {
-           echo '<li><a href="fileUpload.php">Import CSV</a></li>';
+    <li><a href="/">Home</a></li>
+    <li><a href="about.php">About</a></li>
+    <li><a href="#">Contact</a></li>
+    <?php
+        if (isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == 1) {
+            echo '<li><a href="fileUpload.php">Import CSV</a></li>';
         }
-        ?>
-       <li><a href="query.php">Query</a></li>
-       <li><a href="#">Members</a></li>
-       <li><a href="blog.php">Blog</a></li>
-       <?php
-       if (isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == 1) {
+    ?>
+    <li><a href="query.php">Query</a></li>
+    <li><a href="#">Members</a></li>
+    <li><a href="blog.php">Blog</a></li>
+    <?php
+        if (isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == 1) {
            echo '<li><a href="logout.php">Logout</a></li>';
         } else {
-           echo '<li><a href="login.php">Login</a></li>';
+            echo '<li><a href="login.php">Login</a></li>';
         }
-        ?>
+    ?>
     </ul>
 </div>
