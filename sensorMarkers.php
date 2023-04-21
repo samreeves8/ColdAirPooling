c79dc296294301ff83a423e93be11f2d5b075654
KevinCabral401 <KevinCabral401@gmail.com>
2023-04-21 09:32:37 -0600

    rounded

--- sensorMarkers.php
+++ sensorMarkers.php
@@ -1,31 +1,29 @@
 
 <?php
-        $conn = new mysqli('localhost', 'gunniso1_Admin', 'gunnisoncoldair', 'gunniso1_SensorData');
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
+    $conn = new mysqli('localhost', 'gunniso1_Admin', 'gunnisoncoldair', 'gunniso1_SensorData');
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-        $markers = array();
-        
-        $sql = "SELECT Sensor, Latitude, Longitude, Elevation, DATE_FORMAT(Date, '%Y-%m-%d') as Date, humidity FROM SensorData";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute();
-        $result = $stmt->get_result();
-        
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                $markers[] = array(
-                    "id" => $row['Sensor'],
-                    "lat" => $row['Latitude'],
-                    "lng" => $row['Longitude'],
-                    "elevation" => $row['Elevation'],
-                    "dateInstalled" => $row['Date'],
-                    "recordsHumidity" => $row['humidity']
-                );
-            }
+    $markers = array();
+    
+    $sql = "SELECT Sensor, ROUND(Latitude, 2) as Latitude, ROUND(Longitude, 2) as Longitude, Elevation, DATE_FORMAT(Date, '%Y-%m-%d') as Date, humidity FROM SensorData";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $markers[] = array(
+                "id" => $row['Sensor'],
+                "lat" => $row['Latitude'],
+                "lng" => $row['Longitude'],
+                "elevation" => $row['Elevation'],
+                "dateInstalled" => $row['Date'],
+                "recordsHumidity" => $row['humidity']
+            );
         }
-        //print_r($markers);
-        echo json_encode($markers);
-        
-        
-    ?>
+    }
+    
+    echo json_encode($markers);
+?> 
