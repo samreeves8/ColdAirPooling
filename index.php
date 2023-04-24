7c99a819ae2469e6c54a0648299333f61e8330b0
samreeves8 <samreeves8@gmail.com>
2023-04-24 10:13:29 -0600

    Title and logo

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="icon" type="image/png" href="images/Western Logo.png">
+    <title>Home</title>
     <link rel="stylesheet" href="styles/nav.css">
     <link rel = "stylesheet" href = "styles/query.css">
 </head>
