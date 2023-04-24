79248f9b34c82ac2af325321016c1ec2dc665822
samreeves8 <samreeves8@gmail.com>
2023-04-24 10:17:30 -0600

    nav and header to queryResults

--- queryResults.php
+++ queryResults.php
@@ -49,16 +49,8 @@
     <title>Query Results</title>
 </head>
 <body>
-    <div class="navbar">
-         <ul class="menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Contact</a></li>
-            <li><a href="query.php">Query</a></li>
-            <li><a href="#">Members</a></li>
-            <li><a href="login.php">Log In</a></li>
-         </ul>
-    </div>
+<?include 'header.php'?>
+<?include 'navBar.php'?>
 </body>
 </html>
 
