95c8f5847559c63171dabec5066bdb468d22fbac
mwm929 <michaelmw929@gmail.com>
2023-04-24 09:42:26 -0600

    alert

--- blog.php
+++ blog.php
@@ -111,7 +111,7 @@
     }
 
     if (isset($_GET['success'])) {
-        echo 'alert(Posted successfully!)';
+        echo '<p>alert(Posted successfully!)</p>';
     }
 
     $query_main = "SELECT post_id, title, content, member_id FROM BlogPosts LIMIT 5";
