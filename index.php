960e7dfeebed506ae1b8e5e79807141f5492acb7
KevinCabral401 <KevinCabral401@gmail.com>
2023-04-26 17:12:56 -0600

    90

--- index.php
+++ index.php
@@ -19,7 +19,7 @@ session_start();
     <div id="title">
         <h1>Gunnison Cold Air Pooling Project</h1>
     </div>
-    <iframe src="https://www.google.com/maps/d/embed?mid=1QKCUJdVptPLWD9xI8WVnY4fxXUM5NmBl&ehbc=2E312F" width="900" height="480"></iframe>
+    <iframe src="https://www.google.com/maps/d/embed?mid=1QKCUJdVptPLWD9xI8WVnY4fxXUM5NmBl&ehbc=2E312F" width="90%" height="480"></iframe>
 </body>
 </html>
 
