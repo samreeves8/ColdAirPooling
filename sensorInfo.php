03b9d51b98e3d8b981e318042d17786871db1717
KevinCabral401 <KevinCabral401@gmail.com>
2023-04-25 18:48:55 -0600

    added conn and nav/header

--- sensorInfo.php
+++ sensorInfo.php
@@ -1,4 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" href="images/Western Logo.png">
+    <title>Home</title>
+    <link rel="stylesheet" href="styles/nav.css">
+    <link rel = "stylesheet" href = "styles/query.css">
+</head>
+<body>
+    <?php include 'header.php'; ?>
+    <?php include 'navBar.php';?>
+</body>
+</html>
+
 <?php
+
+    //get's connection to database
+    $conn = new mysqli('localhost', 'gunniso1_Admin', 'gunnisoncoldair', 'gunniso1_SensorData');
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
     if($_SERVER['REQUEST_METHOD']==='GET'){
         $sensor =  $_GET['sensor'];
         echo $sensor;
